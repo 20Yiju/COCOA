c481be891cec593da28d024365d15820ddcc6fdd
20Yiju <wjddlwn2637@gmail.com>
2022-07-05 14:49:01 +0900

    Singin.dart

--- SLEProject/lib/main.dart
+++ SLEProject/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:study/func/home.dart';
+import 'package:study/func/Signin.dart';
+//import 'package:study/func/home.dart';
 
 void main() {
   runApp(const MyApp());
