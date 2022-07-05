32c5953b374774721a3ea9f10c766ba335d399ba
rladp127 <rladp127@naver.com>
2022-07-05 16:23:28 +0900

    firebase 1

--- SLEProject/lib/main.dart
+++ SLEProject/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 //import 'package:study/func/Signin.dart';
 //import 'package:study/func/home.dart';
-import 'package:study/func/addStudy.dart';
+import 'package:study/func/add.dart';
 
 void main() {
   runApp( MyApp());
