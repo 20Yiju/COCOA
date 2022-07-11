0b2c3423956cf775681237379e14063834722edb
20Yiju <wjddlwn2637@gmail.com>
2022-07-11 17:25:44 +0900

    schedule

--- SLEProject/lib/main.dart
+++ SLEProject/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+/*import 'package:flutter/material.dart';
 //import 'package:study/func/Signin.dart';
 //import 'package:study/func/addStudy.dart';
 import 'package:study/func/studyInfo.dart';
@@ -6,10 +6,10 @@ import 'package:study/func/studyInfo.dart';
 import 'package:study/func/studyList.dart';
 
 void main() async {
-  runApp(MyApp());
+  runApp(App());
 }
 
-/*class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
 
   Widget build(BuildContext context){
     return MaterialApp(
@@ -21,3 +21,60 @@ void main() async {
     );
   }
 }*/
+
+// Copyright 2019 Aleksander Woźniak
+// SPDX-License-Identifier: Apache-2.0
+
+import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
+
+import 'pages/events_example.dart';
+
+void main() {
+  initializeDateFormatting().then((_) => runApp(MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TableCalendar Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: StartPage(),
+    );
+  }
+}
+
+class StartPage extends StatefulWidget {
+  @override
+  _StartPageState createState() => _StartPageState();
+}
+
+class _StartPageState extends State<StartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('TableCalendar Example'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(height: 12.0),
+            ElevatedButton(
+              child: Text('Events'),
+              onPressed: () => Navigator.push(
+                context,
+                MaterialPageRoute(builder: (_) => TableEventsExample()),
+              ),
+            ),
+            const SizedBox(height: 20.0),
+          ],
+        ),
+      ),
+    );
+  }
+}
